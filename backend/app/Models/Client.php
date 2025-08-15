2b54ff3e725eb76cbc6cf4e231fc6fc0410dc289
Kelvin Custodio <kelvincustodio11@gmail.com>
2025-08-15 12:48:11 -0300

    create model and controller for Client and Address

---
+++ backend/app/Models/Client.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Client extends Model
+{
+    /** @use HasFactory<\Database\Factories\ClientFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'phone',
+        'photo',
+        'age',
+    ];
+
+    public function address() {
+        return $this->hasOne(Address::class);
+    }
+}
